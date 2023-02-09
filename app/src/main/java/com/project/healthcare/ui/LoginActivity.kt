d1768401ec07753e235c9cbe20da2b76df2a7f9c
Candra <ecankurnia@gmail.com>
2023-02-09 22:48:36 +0700

    add authentication

--- app/src/main/java/com/project/healthcare/ui/LoginActivity.kt
+++ app/src/main/java/com/project/healthcare/ui/LoginActivity.kt
@@ -2,11 +2,25 @@ package com.project.healthcare.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.project.healthcare.R
+
+private lateinit var auth: FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+
     }
+
 }
